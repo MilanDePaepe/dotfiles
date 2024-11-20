a4b868e13309eb39d589036a1df21a29e13a22c8
Milan De Paepe <milandepaepe@MacBook-Air-van-Milan.local>
2024-11-20 23:10:54 +0100

    add nvim plugin nvim-java

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -194,6 +194,9 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"nvim-java/nvim-java",
+	},
+	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		opts = {},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
