6082543825b1ba3b98a4b6053c5eacb4fe17a6f8
Milan De Paepe <milandepaepe@MacBook-Air-van-Milan.local>
2024-12-11 21:53:02 +0100

    install oil nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -32,5 +32,7 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Leader + e to open file explore
-vim.keymap.set("n", "<leader>e", ":Ex<CR>")
 vim.keymap.set("n", "<leader>y", ":%y+<CR>")
+
+-- '-' to open oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
