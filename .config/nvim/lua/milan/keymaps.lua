b499cc08d8b63179c43e27c3aebbb6cc720b71df
milan <milandepaepe@icloud.com>
2025-01-05 17:20:14 +0100

    nvim keymap to move around code

--- .config/nvim/lua/milan/keymaps.lua
+++ .config/nvim/lua/milan/keymaps.lua
@@ -27,9 +27,9 @@ vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 --
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 
 -- Leader + e to open file explore
 vim.keymap.set("n", "<leader>y", ":%y+<CR>")
@@ -39,3 +39,7 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Open floating terminal
 vim.keymap.set("n", "<leader>to", "<CMD>Floaterminal<CR>", { desc = "Open floating terminal" })
+
+-- Easy move code around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
